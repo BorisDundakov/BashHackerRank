ff63819c1e29cd6d152ba2347658d67cc31d3594
BorisDundakov <boris.stefan.de@gmail.com>
2023-04-26 11:42:15 +0300

    added cut2.bash

---
+++ cut2.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Read the input lines from a file
+while read line; do
+    # Extract the desired character using cut
+    char=$(echo "$line" | cut -c 2,7)
+    # Print the character
+    echo "$char"
+done 
+
